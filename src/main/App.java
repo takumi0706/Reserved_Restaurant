0980b8ff021bb82ae83f9b82c707b8c9fd37b17a
takumi <ganndamu0706@gmail.com>
2024-09-12 15:07:18 +0900

    feat:Delete package for MissRead

--- src/main/App.java
+++ src/main/App.java
@@ -1,8 +1,8 @@
-package main.track;
+package main;
 
 import java.util.*;
 
-public class Advance {
+public class App {
     public static void main(String[] args) {
         String[] input = getStdin();
         int nTables = Integer.parseInt(input[0].trim());
